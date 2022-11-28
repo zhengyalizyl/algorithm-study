cc1be9eff9c3e74bd1e73f13f2a8c1559b7db092
lizheng <v-lizheng@expediagroup.com>
2022-11-28 22:59:17 +0800

    add valid brackets

--- 递归与栈/有效的括号20.js
+++ 递归与栈/有效的括号20.js
@@ -7,3 +7,48 @@
 // 每个右括号都有一个对应的相同类型的左括号。
 
 // 链接：https://leetcode.cn/problems/valid-parentheses
+
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isValid = function(s) {
+    let arr = [];
+    for (let i = 0; i < s.length; i += 1) {
+        switch (s[i]) {
+            case "(":
+            case "[":
+            case "{":
+                arr.push(s[i])
+                break;
+            case ")":
+                {
+                    if (arr.length === 0 || arr.length > 0 && arr[arr.length - 1] !== '(') {
+                        return false;
+                    }
+                    arr.pop()
+                    break;
+                }
+            case "}":
+                {
+                    if (arr.length === 0 || arr.length > 0 && arr[arr.length - 1] !== '{') {
+                        return false;
+                    }
+                    arr.pop()
+                    break;
+                }
+            case "]":
+                {
+
+                    if (arr.length === 0 || arr.length > 0 && arr[arr.length - 1] !== '[') {
+                        return false;
+                    }
+                    arr.pop();
+                    break;
+                }
+
+
+        }
+    }
+    return arr.length === 0
+};
