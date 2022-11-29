6dc5dad6ea0f58a93a3f9aa17ca8ccadc610a0be
lizheng <v-lizheng@expediagroup.com>
2022-11-29 21:54:06 +0800

    add design-circular-deque

--- 队列/设计实现双端队列641.js
+++ 队列/设计实现双端队列641.js
@@ -14,3 +14,104 @@
 
 // 链接：https://leetcode.cn/problems/design-circular-deque
 // 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+
+/**
+ * @param {number} k
+ */
+var MyCircularDeque = function(k) {
+    this.arr = [];
+    this.temp = k;
+};
+
+/** 
+ * @param {number} value
+ * @return {boolean}
+ */
+MyCircularDeque.prototype.insertFront = function(value) {
+    if (this.isFull()) {
+        return false
+    }
+    this.arr.unshift(value);
+    return true;
+};
+
+/** 
+ * @param {number} value
+ * @return {boolean}
+ */
+MyCircularDeque.prototype.insertLast = function(value) {
+    if (this.isFull()) {
+        return false
+    }
+    this.arr.push(value)
+    return true;
+};
+
+/**
+ * @return {boolean}
+ */
+MyCircularDeque.prototype.deleteFront = function() {
+    if (this.isEmpty()) {
+        return false
+    }
+    this.arr.splice(0, 1)
+    return true
+};
+
+/**
+ * @return {boolean}
+ */
+MyCircularDeque.prototype.deleteLast = function() {
+    if (this.isEmpty()) {
+        return false
+    }
+    this.arr.splice(this.arr.length - 1, 1)
+    return true
+};
+
+/**
+ * @return {number}
+ */
+MyCircularDeque.prototype.getFront = function() {
+    if (this.isEmpty()) {
+        return -1
+    }
+    return this.arr[0]
+};
+
+/**
+ * @return {number}
+ */
+MyCircularDeque.prototype.getRear = function() {
+    if (this.isEmpty()) {
+        return -1
+    }
+    return this.arr[this.arr.length - 1]
+};
+
+/**
+ * @return {boolean}
+ */
+MyCircularDeque.prototype.isEmpty = function() {
+    return this.arr.length == 0;
+};
+
+/**
+ * @return {boolean}
+ */
+MyCircularDeque.prototype.isFull = function() {
+    return this.arr.length == this.temp;
+};
+
+/**
+ * Your MyCircularDeque object will be instantiated and called as such:
+ * var obj = new MyCircularDeque(k)
+ * var param_1 = obj.insertFront(value)
+ * var param_2 = obj.insertLast(value)
+ * var param_3 = obj.deleteFront()
+ * var param_4 = obj.deleteLast()
+ * var param_5 = obj.getFront()
+ * var param_6 = obj.getRear()
+ * var param_7 = obj.isEmpty()
+ * var param_8 = obj.isFull()
+ */
