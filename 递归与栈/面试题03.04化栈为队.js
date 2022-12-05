f9af41bbef044cf973db5e0bec989263af51c63e
lizheng <v-lizheng@expediagroup.com>
2022-12-05 20:14:04 +0800

    add implement-queue-using-stacks-lcci

--- 递归与栈/面试题03.04化栈为队.js
+++ 递归与栈/面试题03.04化栈为队.js
@@ -1,2 +1,51 @@
 // 实现一个MyQueue类，该类用两个栈来实现一个队列。
 // https://leetcode.cn/problems/implement-queue-using-stacks-lcci/
+
+/**
+ * Initialize your data structure here.
+ */
+var MyQueue = function() {
+    this.stack1 = [];
+};
+
+/**
+ * Push element x to the back of queue. 
+ * @param {number} x
+ * @return {void}
+ */
+MyQueue.prototype.push = function(x) {
+    this.stack1.push(x);
+};
+
+/**
+ * Removes the element from in front of queue and returns that element.
+ * @return {number}
+ */
+MyQueue.prototype.pop = function() {
+    return this.stack1.shift();
+};
+
+/**
+ * Get the front element.
+ * @return {number}
+ */
+MyQueue.prototype.peek = function() {
+    return this.stack1[0]
+};
+
+/**
+ * Returns whether the queue is empty.
+ * @return {boolean}
+ */
+MyQueue.prototype.empty = function() {
+    return this.stack1.length === 0
+};
+
+/**
+ * Your MyQueue object will be instantiated and called as such:
+ * var obj = new MyQueue()
+ * obj.push(x)
+ * var param_2 = obj.pop()
+ * var param_3 = obj.peek()
+ * var param_4 = obj.empty()
+ */
