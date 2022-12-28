4cc00825716def7e6b77b758d97bc2c894913445
lizheng <lizheng123lily@timelinelabs.cn>
2022-12-28 09:48:55 +0800

    binary-tree-level

--- 树形结构/ 路径总和112.js
+++ 树形结构/ 路径总和112.js
@@ -4,3 +4,36 @@
 
 // 来源：力扣（LeetCode）
 // 链接：https://leetcode.cn/problems/path-sum
+
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number} targetSum
+ * @return {boolean}
+ */
+var hasPathSum = function(root, targetSum) {
+    if (!root) { return false }
+    //先从左边找，然后从右边找，看有没有满足targetSum的
+    let k = targetSum - root.val;
+    //假设刚好是根结点
+    if (!root.left && !root.right) {
+        return k == 0
+    }
+    if (hasPathSum(root.left, k) != 0) { //如果左边从根到叶子结点(从根到首层)已经满足了，如果为0,说明还未循环到也节点的末尾
+        return true
+    }
+
+    if (hasPathSum(root.right, k) != 0) { //如果右边从根到叶子结点(从根到首层)已经满足了，如果为0,说明还未循环到也节点的末尾
+        return true
+    }
+
+    return false
+};
--- 树形结构/剑指 Offer 54. 二叉搜索树的第k大节点.js
+++ 树形结构/剑指 Offer 54. 二叉搜索树的第k大节点.js
@@ -1,2 +1,48 @@
 // 给定一棵二叉搜索树，请找出其中第 k 大的节点的值。
 // https://leetcode.cn/problems/er-cha-sou-suo-shu-de-di-kda-jie-dian-lcof/
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number} k
+ * @return {number}
+ */
+
+var getCount = function(root) {
+    if (!root) { return 0 }
+    return getCount(root.left) + getCount(root.right) + 1;
+}
+
+
+var getArr = function(root, ans) {
+    if (!root) { return [] }
+    getArr(root.left, ans);
+    ans.push(root.val);
+    getArr(root.right, ans);
+}
+
+var kthLargest = function(root, k) {
+    //方法一:
+    //左根右，中序遍历可以把数据从小到大的排序，
+    //如果k小于右边的个数，说明在右子树，如果大于右边的k树+1，则证明在左子树，如果k等于右边的个数+1，那么说明在根结点  
+    let count = getCount(root.right); //得到右边的个数
+    if (k < count + 1) {
+        return kthLargest(root.right, k) //从右边的子树找
+    }
+    if (k == count + 1) {
+        return root.val //说明是根结点
+    }
+    return kthLargest(root.left, k - count - 1); //从左边子树找
+
+
+    //方法二,将左根右，中序遍历可以把数据从小到大的排序，得到一个数组
+    // let ans=[];
+    // getArr(root,ans);
+    // return ans[ans.length-k];
+};
